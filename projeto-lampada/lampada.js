9434fe87f25efaa599e95bea51abcb821e7cefa6
GPedroza7 <bezerragabriel691@gmail.com>
2023-09-04 20:45:10 -0300

    refresh page

--- projeto-lampada/lampada.js
+++ projeto-lampada/lampada.js
@@ -1,6 +1,7 @@
 const turnOn = document.getElementById('turnOn')
 const turnOff = document.getElementById('turnOff')
 const lamp = document.getElementById('lamp')
+const btn = document.querySelector('#Refresh')
 
 function isLampBroken() {
     return lamp.src.indexOf('quebrada') > -1
@@ -9,23 +10,38 @@ function isLampBroken() {
 
 function lampOn () {
     if (!isLampBroken()){
-    lamp.src='./img/ligada.jpg'
+    lamp.src='./img/ligada.png'
 }
 
 }
 
 function lampOff (){
     if(!isLampBroken()) {
-        lamp.src='./img/desligada.jpg'
+        lamp.src='./img/desligada.png'
     }
 }
 
 
+function lampque (){
+        return lamp.src='./img/quebrada.png'
+    }
+
+function trocar () {
+    if(!isLampBroken ())
+    return btn.addEventListener("click", function() {
+location.reload();
+    });
+}
 
 
 
-
+TurnOn.addEventListener('click', lampOn);
+TurnOff.addEventListener('click', lampOff);
 
 lamp.addEventListener('mouseover', lampOn)
 
 lamp.addEventListener('mouseleave', lampOff)
+
+lamp.addEventListener('dblclick', lampque);
+
+lamp.addEventListener('click', trocar);
